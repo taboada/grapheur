36c6e832c8bbf9ccc994909a629aa6fbf93f0291
Jean-Christophe Taboada <taboada@me.com>
2012-01-05 04:34:17 +0100

    ajouts d'un jeu de fonctions pour gérer du pseudo-random

--- fonctions.js
+++ fonctions.js
@@ -4,10 +4,21 @@ function randInt(max,min) {
   return Math.floor(Math.random() * (max - min) + min); 
 }
 
-function makeRandArray(length){
+function makeRandIntArray(length,min,max){
   var array = [];
   for(i=0; i< length ; i++){
-    array.push(randInt(0,100));
+    array.push(randInt(min,max));
   }
   return array;
+}
+
+function randColor(opacity){
+  return "rgba(" + randInt(0,255)
+                 + ","
+                 + randInt(0,255)
+                 + ","
+                 + randInt(0,255)
+                 + ","
+                 + opacity
+                 + ")"; 
 }
--- tabo.js
+++ tabo.js
@@ -2,19 +2,36 @@
 
 $(document).ready(function() {
 
-  // un petit paquet de valeurs aléatoires
-  var values = makeRandArray(10);
+ // $("#expr").hide();
 
+  // un petit paquet de valeurs aléatoires
+  var values = makeRandIntArray(10,20,80); //nombre, min, max
+  $("#expr").text('valeurs de y : ' + values);
+  // largeur et hauteur de la boite
   var largeur = 500;
   var hauteur = 400;
-  // largeur et hauteur de la boite
   var graphbox = $("#graph");
   graphbox.css("width",largeur);
   graphbox.css("height",hauteur);
 
+  
   var graph = Raphael('graph');
-  graph.piechart(largeur/2,hauteur/2,150,values);
-  //graph.linechart(10,10, 450,270,[1,2,3,4,5],[1,2,3,4,5], { "colors": "red", "symbol": "s", axis: "0 0 1 1"});
- });
+  //graph.piechart(largeur/2,hauteur/2,150,values);
+
+  var options = {
+    "gutter"    : 30,
+    symbol      : "circle",
+    "colors"    : ["rgba(" + randInt(128,255) + ","+ randInt(0,255) +",0,0.8)"],
+    "smooth"    : true,
+    "shade"     : true,
+    "nostroke"  : false,
+    "axis"      : "0 0 1 1"
+  }
+  graph.linechart(0,0,largeur,hauteur,[0,10,20,30,40,50,60,70,80,90],values,options);
+
+  graphbox.fadeIn("slow");
+  $("#expr").delay(1000).fadeIn("slow");
+  $("#msg").delay(1500).fadeIn("slow");
+});
 
 
